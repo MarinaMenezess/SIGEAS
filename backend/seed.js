9f4fa117ad2994ad7988032b910e2cd032542728
Marina Menezes <marinamenezesdeo@gmail.com>
2025-09-29 17:06:31 -0300

    atualização do seed.js

--- backend/seed.js
+++ backend/seed.js
@@ -5,22 +5,22 @@ const bcrypt = require('bcrypt');
 async function run(){
   try {
     // Insere usuários exemplo
-    const senhaAdmin = await bcrypt.hash('admin123', 10);
-    const senhaProf = await bcrypt.hash('prof123', 10);
-    const senhaStu = await bcrypt.hash('stu123', 10);
+    const senhaAdmin = await bcrypt.hash('senha', 10);
+    const senhaProf = await bcrypt.hash('senha', 10);
+    const senhaStu = await bcrypt.hash('senha', 10);
 
-    await pool.query('INSERT INTO usuarios (nome,email,senha,perfil) VALUES (?, ?, ?, ?)', ['Cláudia Pedagoga', 'claudia@sigeas.com', senhaAdmin, 'administrador']);
-    const [r1] = await pool.query('INSERT INTO usuarios (nome,email,senha,perfil) VALUES (?, ?, ?, ?)', ['Marcos Professor', 'marcos@sigeas.com', senhaProf, 'professor']);
+    await pool.query('INSERT INTO usuarios (nome,email,senha,perfil) VALUES (?, ?, ?, ?)', ['Mari', 'mari@sigeas.com', senhaAdmin, 'administrador']);
+    const [r1] = await pool.query('INSERT INTO usuarios (nome,email,senha,perfil) VALUES (?, ?, ?, ?)', ['Arlan', 'arlan@sigeas.com', senhaProf, 'professor']);
     const idProf = r1.insertId;
-    await pool.query('INSERT INTO usuarios (nome,email,senha,perfil) VALUES (?, ?, ?, ?), (?, ?, ?, ?)', ['Ana Aluna','ana@sigeas.com',senhaStu,'aluno','Lucas Aluno','lucas@sigeas.com',senhaStu,'aluno']);
+    await pool.query('INSERT INTO usuarios (nome,email,senha,perfil) VALUES (?, ?, ?, ?), (?, ?, ?, ?)', ['Marina','marina@sigeas.com',senhaStu,'aluno']);
 
     // criar turma
-    const [tm] = await pool.query('INSERT INTO turmas (nome, descricao, ano) VALUES (?, ?, ?)', ['Matemática - 3º Ano', 'Matemática', 2025]);
+    const [tm] = await pool.query('INSERT INTO turmas (nome, descricao, ano) VALUES (?, ?, ?)', ['3BM', 'Turma mais legal', 2025]);
     const idTurma = tm.insertId;
 
     // associar professor e matricular alunos
     // CORREÇÃO: Adicionando 'materia' que é agora obrigatório
-    await pool.query('INSERT INTO professores_turmas (id_professor, id_turma, materia) VALUES (?, ?, ?)', [idProf, idTurma, 'matematica']);
+    await pool.query('INSERT INTO professores_turmas (id_professor, id_turma, materia) VALUES (?, ?, ?)', [idProf, idTurma, 'filosofia']);
 
     // pega ids dos alunos
     const [alunos] = await pool.query('SELECT id_usuario FROM usuarios WHERE perfil = "aluno"');
